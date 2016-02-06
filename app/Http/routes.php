da256a4d1b79080836dc0f180fd86ff27aaad07f
Ben Argo <ben@benargo.com>
2016-02-06 22:00:33 +0000

    Added legacy routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -21,3 +21,37 @@ Route::controllers([
 	'auth' => 'Auth\AuthController',
 	'password' => 'Auth\PasswordController',
 ]);
+
+/*
+|--------------------------------------------------------------------------
+| Old URLs
+|--------------------------------------------------------------------------
+|
+| These URLs were used by my portfolio in versions prior to version 10 (this
+| version). They have been referenced below so we can provide proper HTTP
+| status codes for them.
+|
+*/
+
+/*
+|--------------------------------------------------------------------------
+| Media Subdomain
+|--------------------------------------------------------------------------
+|
+| A while ago I used to contain media assets under a seperate subdomain of
+| 'media.benargo.net.' These days that subdomain doesn't exist, but the
+| following routes are a legacy of those days.
+|
+*/
+Route::group(['domain' => 'media.benargo.net'], function()
+{
+	Route::get('/templates/misc/reset.css', function()
+	{
+		return redirect('http://v6.benargo.com/media/templates/misc/reset.css');
+	});
+
+	Route::get('/scripts/jquery-1.5.1.min.js', function()
+	{
+			return redirect('http://code.jquery.com/jquery-1.5.1.min.js');
+	});
+});
